c16574ed055c7d3e32feeebc14c1126902fd1b5e
Riya Jain <riya1jain567@gmail.com>
2024-05-04 18:49:38 +0530

    Added video link

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -6,14 +6,34 @@ import Hero from "./components/Hero";
 export default function LandingPage() {
   return (
     <section className="relative w-screen">
-      <div className="border-b pb-6 border-front/20 flex absolute pt-6 items-center w-full text-lg mobile:text-base mobile:gap-x-3 mobile:py-4
-      -tracking-wide justify-center gap-x-8">
+      <div
+        className="border-b pb-6 border-front/20 flex absolute pt-6 items-center w-full text-lg mobile:text-base mobile:gap-x-3 mobile:py-4
+      -tracking-wide justify-center gap-x-8"
+      >
         {navLinks.map((navLink, i) => (
           <>
-            <Link to={navLink.to} className="cursor-pointer">{navLink.title}</Link>
-            {navLinks.length -1 > i && <span className="text-3xl mobile:text-xl">•</span>}
+            <Link to={navLink.to} target={`_newABC`} className="cursor-pointer">
+              {navLink.title}
+            </Link>
+            <span className="text-3xl mobile:text-xl">•</span>
           </>
         ))}
+        <Link
+          to="/Amex_Hackathon.pdf"
+          target={`_newABC`}
+          className="cursor-pointer"
+        >
+          PPT
+        </Link>
+
+        <span className="text-3xl mobile:text-xl">•</span>
+        <Link
+          to="https://youtu.be/ZniI5oX7PiA"
+          target={`_newABC`}
+          className="cursor-pointer"
+        >
+          Video
+        </Link>
       </div>
       <ThemeButton className="absolute top-4 right-8 mobile:top-3 text-2xl mobile:text-xl mobile:right-4 p-2 aspect-square bg-foreground rounded-full flex justify-center items-center border border-front border-opacity-40" />
       <Hero />
@@ -24,14 +44,6 @@ export default function LandingPage() {
 const navLinks = [
   {
     title: "Home",
-    to: "/",
-  },
-  {
-    title: "PPT",
-    to: "/",
-  },
-  {
-    title: "Video",
     to: "/",
   },
   {
--- src/pages/TeamPage/TeamPage.tsx
+++ src/pages/TeamPage/TeamPage.tsx
@@ -4,20 +4,34 @@ import ThemeButton from "../../common/ThemeButton";
 export default function TeamPage() {
   return (
     <section className="relative w-screen">
-      <div
+   <div
         className="border-b pb-6 border-front/20 flex absolute pt-6 items-center w-full text-lg mobile:text-base mobile:gap-x-3 mobile:py-4
       -tracking-wide justify-center gap-x-8"
       >
         {navLinks.map((navLink, i) => (
           <>
-            <Link to={navLink.to}  target={`_newABC`} className="cursor-pointer">
+            <Link to={navLink.to} target={`_newABC`} className="cursor-pointer">
               {navLink.title}
             </Link>
-            {navLinks.length - 1 > i && (
-              <span className="text-3xl mobile:text-xl">•</span>
-            )}
+            <span className="text-3xl mobile:text-xl">•</span>
           </>
         ))}
+        <Link
+          to="/Amex_Hackathon.pdf"
+          target={`_newABC`}
+          className="cursor-pointer"
+        >
+          PPT
+        </Link>
+
+        <span className="text-3xl mobile:text-xl">•</span>
+        <Link
+          to="https://youtu.be/ZniI5oX7PiA"
+          target={`_newABC`}
+          className="cursor-pointer"
+        >
+          Video
+        </Link>
       </div>
       <ThemeButton className="absolute top-4 right-8 mobile:top-3 text-2xl mobile:text-xl mobile:right-4 p-2 aspect-square bg-foreground rounded-full flex justify-center items-center border border-front border-opacity-40" />
       <div className="p-page py-28 flex flex-col items-center">
@@ -26,7 +40,7 @@ export default function TeamPage() {
           {team.map((member, i) => (
             <div
               key={i}
-              className="w-[28%] mobile:w-[35%] bg-primary/10 rounded-xl flex flex-col items-center px-2 py-6 border border-primary/40"
+              className="w-[40%] mobile:w-[35%] bg-primary/10 rounded-xl flex flex-col items-center px-2 py-6 border border-primary/40"
             >
               <img src={member.imgUrl} className="w-[8vw] rounded-full mobile:w-[16vw] aspect-square object-cover" />
               <h1 className="mt-4 text-lg font-bold">{member.name}</h1>
@@ -60,14 +74,6 @@ const team = [
 const navLinks = [
   {
     title: "Home",
-    to: "/",
-  },
-  {
-    title: "PPT",
-    to: "/Amex_hackathon.pdf",
-  },
-  {
-    title: "Video",
     to: "/",
   },
   {
